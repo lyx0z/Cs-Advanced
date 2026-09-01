c2483f9f58f9af641cf9ffdd9726e04e9b555ced
Kucher Mykyta Serhiyovich <mykyta.kucher@digitecgalaxus.ch>
2026-09-01 13:51:31 +0200

    ToHashSet()

--- 01-Collections/Program.cs
+++ 01-Collections/Program.cs
@@ -4,6 +4,18 @@ public class Collections
 {
     public static void Main()
     {
-        IList<int> nums = new List<int>() { 1, 2, 3, 4, 5, 6, 6, 6, 7, 8, 2, 3, 1, 4 };
+        var nums = new List<int>() { 1, 2, 3, 4, 5, 6, 6, 6, 7, 8, 2, 3, 1, 4 };
+        var hashSet = ListToHashSet(nums);
+
+        foreach (var num in hashSet)
+        {
+            Console.WriteLine(num);
+        }
+    }
+
+    private static HashSet<int> ListToHashSet(List<int> nums)
+    {
+        var hashSet = nums.ToHashSet();
+        return hashSet;
     }
 }
