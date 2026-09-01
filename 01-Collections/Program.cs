a760987ef2369e371bfea8d18b13a5fb5d135673
Kucher Mykyta Serhiyovich <mykyta.kucher@digitecgalaxus.ch>
2026-09-01 11:47:57 +0200

    new solution

---
+++ 01-Collections/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _01_Collections;
+
+public class Collections
+{
+    public static void Main()
+    {
+        IList<int> nums = new List<int>() { 1, 2, 3, 4, 5, 6, 6, 6, 7, 8, 2, 3, 1, 4 };
+    }
+}
